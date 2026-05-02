1467a2b2ab95a552e43f0c09b426c076741b23f2
AbdelRahman Alnajjar <>
2026-05-01 19:21:05 -0700

    Add DynamoDB integration and POST /orders endpoint

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// AWS dynamoDB
+// import AWS SDK
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { v4: uuidv4 } = require("uuid");
+
+
+//create DynamoDB client
+const client = new DynamoDBClient({ region: "us-west-1" });
+const docClient = DynamoDBDocumentClient.from(client);
+
 // Load product data
 const products = require("./data/products.json");
 
@@ -26,6 +37,39 @@ app.get("/", (req, res) => {
     products
   });
 });
+
+
+//  /orders route
+app.post("/orders", async (req, res) => {
+  try {
+    const { name, item } = req.body;
+
+    if (!name || !item) {
+      return res.status(400).json({ error: "Missing fields" });
+    }
+
+    const order = {
+      orderId: uuidv4(),
+      name,
+      item,
+      createdAt: new Date().toISOString()
+    };
+
+    await docClient.send(
+      new PutCommand({
+        TableName: "orders",
+        Item: order
+      })
+    );
+
+    res.status(201).json({ message: "Order saved", order });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to save order" });
+  }
+});
+
 
 // Start server
 app.listen(PORT, "0.0.0.0", () => {
